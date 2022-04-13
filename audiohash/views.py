dd638fe7c9d71f112ea1adc38b0a3a78f6c5cd10
Maaz Adil <maazadil786@gmail.com>
2022-04-13 12:21:02 -0700

    added hashtable with basic hash function for testing

--- audiohash/views.py
+++ audiohash/views.py
@@ -3,7 +3,53 @@ from django.urls import reverse
 
 
 import requests
+import speech_recognition as sr
+import pyttsx3
 
 # Create your views here.
 def index(request):
-    return render(request, "audiohash/main.html")
+
+    r = sr.Recognizer()
+
+    with sr.AudioFile('/Users/mymac/Downloads/lexclip.wav') as source:
+        audio = r.listen(source)
+        try:
+            text = r.recognize_google(audio)
+            print("Working on...")
+            print(text)
+        except:
+            print("Sorry.. run again...")
+
+    HashTable = [[] for _ in range(10)]
+
+    def Hashing(keyValue):
+        return ord(keyValue) # % len(HashTable)
+
+    def insert(Hashtable, keyValue, value):
+        hash_key = Hashing(keyValue)
+        Hashtable[hash_key].append(value)
+
+    def display_hash(hashTable):
+        for i in range(len(hashTable)):
+            print(i, end = " ")
+            
+            for j in hashTable[i]:
+                print("-->", end = " ")
+                print(j, end = " ")
+                
+            print()
+
+    res = text.split()
+    key = 0
+
+    for i in res:
+        key += 1
+        insert(HashTable, i, key)
+    
+    display_hash(HashTable)
+        
+
+    return render(request, "audiohash/main.html", {
+        "text": text,
+        "hash": HashTable
+    })
